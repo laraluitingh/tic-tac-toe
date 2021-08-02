1524a222d13677627e62183d8c5d0f6bc1411851
laraluitingh <61213237+laraluitingh@users.noreply.github.com>
2021-08-02 20:57:27 +1000

    UI changes

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ let players = {
   },
 };
 
-
-
 // const winningconditons=[
 //  [0,1,2],
 //  [3,4,5],
@@ -23,90 +21,108 @@ let players = {
 //  [2,4,6]
 // ]
 
-
 let startButton = document.getElementById("start-game");
 let player1Name = document.getElementById("player1");
 let player2Name = document.getElementById("player2");
-let player1Plays=[];
-let player2Plays=[];
+let player1Info = document.getElementById("player1-info");
+let player2Info = document.getElementById("player2-info");
+const winningDiv=document.getElementById("bottom-container");
+
+let player1Plays = [];
+let player2Plays = [];
 
 startButton.addEventListener("click", (event) => {
-  if(players.player1.turn==false && players.player2.turn==false){
-    players.player1.turn=true
+  if (players.player1.turn == false && players.player2.turn == false) {
+    players.player1.turn = true;
     player1Name.classList.add("turn");
-    console.log(players)
-  }else {
-    alert("a game is already in progress")
-  } 
+    console.log(players);
+    startButton.remove();
+  } else {
+    alert("a game is already in progress");
+  }
 });
 
-
-const blocks=document.getElementsByClassName("block");
-for(let block of blocks){
+const blocks = document.getElementsByClassName("block");
+for (let block of blocks) {
   block.addEventListener("click", blockSelected);
 }
 
-function blockSelected(event){
+function blockSelected(event) {
   console.log(event.target);
-  const divTarget=event.target
-   if(players.player1.turn && divTarget.classList.contains("oselected")==false && divTarget.classList.contains("xselected")==false){
-     console.log("succesful");
-       divTarget.classList.add("oselected");
-       players.player1.turn=false;
-       players.player2.turn=true;
-       player1Name.classList.remove("turn");
-       player2Name.classList.add("turn");
-       console.log("succesful");
-       console.log(event.target);
-       let newNumer=parseInt(divTarget.getAttribute("data-index"));
-       player1Plays.push(newNumer);
-       console.log(player1Plays);
-       winning();
-
-   }else if(players.player2.turn && divTarget.classList.contains("oselected")==false && divTarget.classList.contains("xselected")==false){
+  const divTarget = event.target;
+  if (
+    players.player1.turn &&
+    divTarget.classList.contains("oselected") == false &&
+    divTarget.classList.contains("xselected") == false
+  ) {
+    console.log("succesful");
+    divTarget.classList.add("oselected");
+    players.player1.turn = false;
+    players.player2.turn = true;
+    player1Name.classList.remove("turn");
+    player2Name.classList.add("turn");
+    console.log("succesful");
+    console.log(event.target);
+    let newNumer = parseInt(divTarget.getAttribute("data-index"));
+    player1Plays.push(newNumer);
+    console.log(player1Plays);
+    winning();
+  } else if (
+    players.player2.turn &&
+    divTarget.classList.contains("oselected") == false &&
+    divTarget.classList.contains("xselected") == false
+  ) {
     divTarget.classList.add("xselected");
-    players.player2.turn=false;
-    players.player1.turn=true;
+    players.player2.turn = false;
+    players.player1.turn = true;
     player2Name.classList.remove("turn");
     player1Name.classList.add("turn");
     console.log("succesful");
     console.log(event.target);
-    let newNumber2=parseInt(divTarget.getAttribute("data-index"));
+    let newNumber2 = parseInt(divTarget.getAttribute("data-index"));
     player2Plays.push(newNumber2);
     console.log(player2Plays);
     winning();
-   }
-
+  }
 }
 
+function winning() {
+  const winningconditons = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < winningconditons.length; i++)
+    if (
+      player1Plays.includes(winningconditons[i][0]) &&
+      player1Plays.includes(winningconditons[i][1]) &&
+      player1Plays.includes(winningconditons[i][2])
+    ) {
+      console.log("player 1 wins");
+      let winner=document.createElement("p");
+      console.log(winningDiv)
+      winner.innerText="Player 1 wins"
+      winner.classList.add("winner");
+      console.log(winner);
+      winningDiv.append(winner);
+    } else if (
+      player2Plays.includes(winningconditons[i][0]) &&
+      player2Plays.includes(winningconditons[i][1]) &&
+      player2Plays.includes(winningconditons[i][2])
+    ) {
+      console.log("player 2 wins");
+      let winner2=document.createElement("p");
+      let div=document.getElementById("center-button");
+      console.log(winningDiv)
+      winner2.innerText="Player 2 wins";
+      winner2.classList.add("winner");
+      console.log(winner2);
+      winningDiv.append(winner2);
 
-
-
-
-function winning(){
-  const winningconditons=[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-   ]
-  for(let i=0; i<winningconditons.length; i++)
-   if(player1Plays.includes(winningconditons[i][0]) && player1Plays.includes(winningconditons[i][1]) && player1Plays.includes(winningconditons[i][2])){
-     console.log("player 1 wins")
-   }else if(player2Plays.includes(winningconditons[i][0]) && player2Plays.includes(winningconditons[i][1]) && player2Plays.includes(winningconditons[i][2])){
-    console.log("player 2 wins")
-   }
-
-
-
-
+    }
 }
-
-
-
-
-
