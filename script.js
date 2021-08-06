31c84c8d4055dc2103aaa40039c9c6786f7a051b
laraluitingh <61213237+laraluitingh@users.noreply.github.com>
2021-08-06 16:54:17 +1000

    changes

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ let players = {
   },
 };
 
-
 let startButton = document.getElementById("start-game");
 
 //username of player1
@@ -21,22 +20,29 @@ let player1Name = document.getElementById("player1");
 //username of player2
 let player2Name = document.getElementById("player2");
 
-//all of player one's info their score, their name, their div
+//all of player one's info :their scores, their names, and signs in a div
 let player1Info = document.getElementById("player1-info");
 let player2Info = document.getElementById("player2-info");
+//containe in which start button and play again button isin
 const winningDiv = document.getElementById("bottom-container");
+//keep tracks of the players scores
 let player1Score = 0;
 let player2Score = 0;
+//player scores in the front-end
 let player1ScorePage = document.getElementById("player-1-score");
 let player2ScorePage = document.getElementById("player-2-score");
+//keep track of the players turns
 let plays = [];
+
+//Keeps track if there is a winner in the rounf
 let winnerRound = false;
 
-let audioContext = new AudioContext();
+//keep track of the players plays- each box in grid has an element associated to it
 
 let player1Plays = [];
 let player2Plays = [];
 
+//starts the game
 startButton.addEventListener("click", (event) => {
   if (players.player1.turn == false && players.player2.turn == false) {
     players.player1.turn = true;
@@ -49,13 +55,15 @@ startButton.addEventListener("click", (event) => {
   }
 });
 
+//add event listeners to box in the gird
 const blocks = document.getElementsByClassName("block");
 for (let block of blocks) {
   block.addEventListener("click", blockSelected);
 }
 
-let counter = 0;
+//let counter = 0;
 
+//checks which block is selected using html attributes
 function blockSelected(event) {
   console.log(event.target);
   const divTarget = event.target;
@@ -65,11 +73,15 @@ function blockSelected(event) {
     divTarget.classList.contains("xselected") == false
   ) {
     console.log("succesful");
+
+    //check which sign the player has chosen
     if (players.player1.sign === "naughts") {
       divTarget.classList.add("oselected");
     } else {
       divTarget.classList.add("xselected");
     }
+
+    //changes turns
     players.player1.turn = false;
     players.player2.turn = true;
     player1Name.classList.remove("turn");
@@ -77,11 +89,14 @@ function blockSelected(event) {
     console.log("succesful");
     console.log(event.target);
     let newNumer = parseInt(divTarget.getAttribute("data-index"));
+    //keep track tof th player play by recording blocks index
     player1Plays.push(newNumer);
-    console.log(player1Plays.length);
-    console.log(player1Plays);
-    counter++;
+    //console.log(player1Plays.length);
+    //console.log(player1Plays);
+    //counter++;
+    //keeps track of when the players went will be used to determine a draw
     plays.push("player1");
+    //check if the player has won
     winning();
   } else if (
     players.player2.turn &&
@@ -104,11 +119,13 @@ function blockSelected(event) {
     console.log(player2Plays.length);
     console.log(player2Plays);
     plays.push("player2");
+    //check if the player has won
     winning();
   }
 }
 
 function winning() {
+  //winning condtions represents each block's index
   const winningconditons = [
     [0, 1, 2],
     [3, 4, 5],
@@ -119,6 +136,7 @@ function winning() {
     [0, 4, 8],
     [2, 4, 6],
   ];
+  //loops through each array in the index to determine if the player has won
   for (let i = 0; i < winningconditons.length; i++) {
     if (
       player1Plays.includes(winningconditons[i][0]) &&
@@ -128,7 +146,7 @@ function winning() {
       console.log("player 1 wins");
       let winner = document.createElement("p");
       console.log(winningDiv);
-      winner.innerText = "Player 1 wins";
+      winner.innerText = `${players.player1.userName} wins`;
       winner.classList.add("winner");
       console.log(winner);
       winningDiv.append(winner);
@@ -138,23 +156,29 @@ function winning() {
       winningDiv.append(playAgainButton);
       //playAgainButton.setAttribute("id", "playAgain");
       player1Score++;
+      //update the score
       player1ScorePage.innerText = player1Score;
       playAgainButton.addEventListener("click", playAgain);
+      //remove event listners from the block
       const blocks = document.getElementsByClassName("block");
       for (let block of blocks) {
         block.removeEventListener("click", blockSelected);
       }
 
+      //removes turn class
       if (player1Name.classList.contains("turn")) {
         player1Name.classList.remove("turn");
       } else if (player2Name.classList.contains("turn")) {
         player2Name.classList.remove("turn");
       }
 
+      //if player one win , then player 1 will go first in the next round
       players.player1.turn = true;
       players.player2.turn = false;
+      //resets arrays
       plays = [];
 
+      //there is a winner in this round
       winnerRound = true;
 
       break;
@@ -166,7 +190,7 @@ function winning() {
       console.log("player 2 wins");
       let winner2 = document.createElement("p");
       console.log(winningDiv);
-      winner2.innerText = "Player 2 wins";
+      winner2.innerText = `${players.player2.userName} wins`;
       winner2.classList.add("winner");
       console.log(winner2);
       winningDiv.append(winner2);
@@ -199,6 +223,7 @@ function winning() {
     }
   }
 
+  //checks if there is a winner in this round and checks if all the blocks have been played
   if (
     player1Plays.length + player2Plays.length === 9 &&
     winnerRound === false
@@ -224,6 +249,8 @@ function winning() {
 
     console.log(plays);
 
+    // if player one went first in the previous round they will go again in the next round
+    //if player two went first in the prveious rounf they will go again in the next round
     if (plays[0] === "player1") {
       players.player1.turn = true;
       players.player2.true = false;
@@ -244,9 +271,11 @@ function playAgain() {
   // let winningPlayer=buttonPlay.sib
   // console.log(winningPlayer);
   winnerRound = false;
+  //removes play again button and winner text
   document.getElementsByClassName("winner")[0].remove();
   document.getElementsByClassName("play-again")[0].remove();
 
+  //add turn class if the if it the players turn
   if (players.player1.turn) {
     player1Name.classList.add("turn");
   } else if (players.player2.turn) {
@@ -255,9 +284,12 @@ function playAgain() {
 
   const blocks = document.getElementsByClassName("block");
   for (let block of blocks) {
+    //add events listeners to the block
     block.addEventListener("click", blockSelected);
+    //empties the players plays
     player1Plays = [];
     player2Plays = [];
+    //removes the elemets from the blocks
     if (block.classList.contains("oselected")) {
       block.classList.remove("oselected");
     } else if (block.classList.contains("xselected")) {
@@ -266,14 +298,17 @@ function playAgain() {
   }
 }
 
+//displays the pop-up
 document.getElementById("edit").addEventListener("click", (event) => {
   document.querySelector(".pop-up").style.display = "flex";
 });
 
+//closes the pop-up
 document.getElementById("close").addEventListener("click", (event) => {
   document.querySelector(".pop-up").style.display = "none";
 });
 
+//records values into the objects
 document.getElementById("submit-form").addEventListener("click", (event) => {
   event.preventDefault();
 
@@ -290,6 +325,7 @@ document.getElementById("submit-form").addEventListener("click", (event) => {
   // players.player2.userName=document.getElementById("player2UserName").value;
   // players.player2.sign=document.getElementById("player-2-sign").value;
 
+  //Checks if usernames and signs are unique
   if (player1Value === player2Value || player1Sign === player2Sign) {
     document.querySelector(".error-handler").style.display = "flex";
     document.getElementById("error-message").innerText =
@@ -300,7 +336,11 @@ document.getElementById("submit-form").addEventListener("click", (event) => {
 
     // Calculate time out period then execute the command
     var timeOutPeriod = waitseconds * 1000;
+
+  
     setTimeout(strCmd, timeOutPeriod);
+
+    //checks wther the userNames have been provided
   } else if (player1Value.length === 0 || player2Value.length === 0) {
     document.querySelector(".error-handler").style.display = "flex";
     document.getElementById("error-message").innerText =
@@ -334,6 +374,8 @@ document.getElementById("submit-form").addEventListener("click", (event) => {
     }
   }
 });
+
+//sets values to default by reloading the page 
 
 document.getElementById("restart").addEventListener("click", (event) => {
   window.location.reload();
